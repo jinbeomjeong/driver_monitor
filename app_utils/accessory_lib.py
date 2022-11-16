1c2b2e3014fe0c126c9c4f43b6ff87196aea185a
jinbeom, jeong <ppgt1225@naver.com>
2022-11-16 22:27:58 +0900

    update function of pytorch environment status

--- app_utils/accessory_lib.py
+++ app_utils/accessory_lib.py
@@ -8,6 +8,6 @@ def pytorch_system_info():
     print('Pytorch Version :', torch.__version__) 
     print("Open Computer Vision Version :", cv2.__version__) 
     print('GPU Available :', torch.cuda.is_available())
-    print('CUDNN Available: ', torch.backends.cudnn.is_available())
+    print('CUDNN Available : ', torch.backends.cudnn.is_available())
     print('GPU Device Name :', torch.cuda.get_device_name(0)) 
     print('Number of GPU :', torch.cuda.device_count(), '\n')
